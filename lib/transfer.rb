01643b71758a35cfed3c1ee4ce669aff96f0f1da
Eric Chrobak <eric@ericchrobak.com>
2018-06-14 00:29:06 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,6 +25,7 @@ class Transfer
         @status = "complete"
       else
         "Transaction rejected. Please check your account balance."
+        @status = "rejected"
       end
     end
   end
